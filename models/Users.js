00022b5551b7bdb306e3fd3ec0d6447b85cb0b71
jung su young <j9972@naver.com>
2022-03-04 19:30:20 +0900

    set up[ of Database by sequelize

---
+++ models/Users.js
@@ -0,0 +1,43 @@
+module.exports = (sequelize, DataTypes) => {
+  const Users = sequelize.define(
+    "Users",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        comment: "고유번호 UUID",
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        comment: "유저 닉네임",
+      },
+      email: {
+        type: DataTypes.STRING,
+        validate: {
+          isEmail: true,
+        },
+        allowNull: false,
+        comment: "이메일",
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        comment: "비밀번호",
+      },
+      refreshTokens: {
+        type: DataTypes.STRING,
+        comment: "유저의 refresh token",
+      },
+    },
+    {
+      charset: "utf8", // 한국어 설정
+      collate: "utf8_general_ci", // 한국어 설정
+      timestamps: true, // createAt & updateAt 활성화
+      paranoid: true, // timestamps 가 활성화 되어야 사용 가능 > deleteAt 옵션 on
+    }
+  );
+
+  return Users;
+};
