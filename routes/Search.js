eed9571cd34b786a58706c80569f1f801322f58e
jung su young <j9972@naver.com>
2022-03-21 22:40:37 +0900

    change a bit of code

--- routes/Search.js
+++ routes/Search.js
@@ -1,24 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const bodyParser = require("body-parser");
 const axios = require("axios");
 const uuidAPIKey = require("uuid-apikey");
 
 // console.log(uuidAPIKey.create()); -> 시스템마다 다른 api키를 제공하면 이값들을 디비에서 관리하면 된다 -> isAPIKey method or check method로 확인가능
 
-router.use(express.urlencoded({ extended: true }));
-router.use(bodyParser.json());
+router.use(express.urlencoded({ extended: false }));
+router.use(express.json());
 require("dotenv").config();
 
-router.get("/", (req, res) => {
-  //const word = req.body;
-  const { word } = req.query.query;
-  console.log("word: ", word);
+router.post("/search", (req, res) => {
+  const search = req.body.search;
+  console.log("search:", search);
+
   axios
     .get("https://openapi.naver.com/v1/search/local.json", {
       params: {
-        query: word,
-        display: 2,
+        query: search,
+        display: 1,
         start: 1,
         sort: "random",
       },
@@ -26,15 +25,68 @@ router.get("/", (req, res) => {
         "X-Naver-Client-Id": process.env.NAVER_LOCAL_ID_KEY,
         "X-Naver-Client-Secret": process.env.NAVER_LOCAL_SECRET_KEY,
         "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
       },
     })
     .then((response) => {
-      const item = response.data.items;
-      res.json(item);
+      if (response.status === 200) {
+        const items = response.data.items;
+        items.map((x) => {
+          x.title = x.title.replace(/<b>/g, "");
+          x.title = x.title.replace(/<\/b>/g, "");
+        });
+        res.json(items);
+      }
     })
     .catch((error) => {
       res.json({ msg: error });
+      console.log("err:", error);
     });
+});
+
+router.post("/photo", async (req, res) => {
+  const search = req.body.search;
+  console.log("search:", search);
+
+  //const data = naverLocalApi.search(search);
+
+  axios
+    .get("https://openapi.naver.com/v1/search/image.json", {
+      params: {
+        query: search,
+        display: 1,
+        start: 1,
+        sort: "sim",
+        filter: "small",
+      },
+      headers: {
+        "X-Naver-Client-Id": process.env.NAVER_LOCAL_ID_KEY,
+        "X-Naver-Client-Secret": process.env.NAVER_LOCAL_SECRET_KEY,
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+    })
+    .then((response) => {
+      if (response.status === 200) {
+        const items = response.data.items;
+        items.map((x) => {
+          x.title = x.title.replace(/<b>/g, "");
+          x.title = x.title.replace(/<\/b>/g, "");
+          // <b> 없애줌
+          // 참고로 replace 메서드는 첫번재 파라미터가 리터럴일 경우 일치하는 첫번째 부분만 변경하기 때문에 전부 찾을 수 있도록 정규표현식으로 g를 포함
+        });
+        res.json(items);
+      }
+    })
+    .catch((error) => {
+      console.log("error:", error);
+    });
+
+  //res.json('success connect with server');
+});
+
+router.get("/", (req, res) => {
+  res.json("hello");
 });
 
 module.exports = router;
